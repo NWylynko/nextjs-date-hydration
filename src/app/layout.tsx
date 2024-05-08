0ecf549e349dc43317007f4485ffbc93521bff42
Nick Wylynko <nick@wylynko.com>
2024-05-08 23:59:46 +0800

    Add plausible analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Plausible from "next-plausible";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Plausible
+          domain="date-hydration.vercel.app"
+          enabled={process.env.NODE_ENV === "production"}
+        />
+      </body>
     </html>
   );
 }
